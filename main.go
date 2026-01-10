0eab88af0557aaf3480edb987135df5f5f24eaf8
Alexey Yaroshenko <alexy.yaroshenko05@icloud.com>
2026-01-10 20:56:06 +0300

    headers4create and rul.Value4show

--- main.go
+++ main.go
@@ -1,19 +1,36 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	w.Write([]byte("Hello from snpbox"))
 }
 
 func snippetShow(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Showing snippet..."))
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Snippet with id: %d", id)
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
 	w.Write([]byte("Creating snippet..."))
 }
 
