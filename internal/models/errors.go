080864b15d6149b4fea5af5af5df161a899bd914
Alexey Yaroshenko <alexy.yaroshenko05@icloud.com>
2026-02-09 21:20:06 +0300

    user model and errors

--- internal/models/errors.go
+++ internal/models/errors.go
@@ -3,3 +3,7 @@ package models
 import "errors"
 
 var ErrNoRecord = errors.New("models: no matching record found")
+
+var ErrDuplicateEmail = errors.New("models: this email is used")
+
+var ErrInvalidCredentials = errors.New("models: invalid credentials")
