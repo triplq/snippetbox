3d69c413cc053a9eb0d1420cb2d079c588e964f9
Alexey Yaroshenko <alexy.yaroshenko05@icloud.com>
2026-02-09 21:05:11 +0300

    better users auth routes and nav.html

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -30,11 +30,11 @@ func routes(app *application.Application) http.Handler {
 	mux.Handle("GET /snippets/view/{id}", dynamicChain.ThenFunc(handlers.ShowSnippets(app)))
 	mux.Handle("GET /snippets/create", dynamicChain.ThenFunc(handlers.CreateSnippet(app)))
 	mux.Handle("POST /snippets/create", dynamicChain.ThenFunc(handlers.PostCreateSnippet(app)))
-	mux.Handle("GET /snippets/login", dynamicChain.ThenFunc(handlers.LogIn(app)))
-	mux.Handle("POST /snippets/login", dynamicChain.ThenFunc(handlers.LogInPost(app)))
-	mux.Handle("GET /snippets/signup", dynamicChain.ThenFunc(handlers.SignUp(app)))
-	mux.Handle("POST /snippets/signup", dynamicChain.ThenFunc(handlers.SignUp(app)))
-	mux.Handle("POST /snippets/logout", dynamicChain.ThenFunc(handlers.LogOut(app)))
+	mux.Handle("GET /user/login", dynamicChain.ThenFunc(handlers.LogIn(app)))
+	mux.Handle("POST /user/login", dynamicChain.ThenFunc(handlers.LogInPost(app)))
+	mux.Handle("GET /user/signup", dynamicChain.ThenFunc(handlers.SignUp(app)))
+	mux.Handle("POST /user/signup", dynamicChain.ThenFunc(handlers.SignUp(app)))
+	mux.Handle("POST /user/logout", dynamicChain.ThenFunc(handlers.LogOut(app)))
 
 	return mux
 }
